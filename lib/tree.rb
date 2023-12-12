a38af67b5d12138fb3ba42c9a6e22924eeaff905
John <johnfarris217@gmail.com>
2023-12-12 15:46:03 -0600

    commit tree 2ceb0138dedef24f896e3fca104d955af350c896

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,19 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    result = nil
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == id
+        result = node
+      end
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+    result
+  end
 end
+
